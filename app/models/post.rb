c579d30adaf39774a56c3a30e749aa51d87ca231
dbc08 <apprentice@dbc08.local>
2013-05-11 20:45:08 -0700

    Tables links creation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ActiveRecord::Base
   # Remember to create a migration!
+  has_many :posts_tags
+  has_many :tags, through: :posts_tags
+
+  validates :title, :content, presence: true
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,7 @@
 class Tag < ActiveRecord::Base
   # Remember to create a migration!
+  has_many :posts_tags
+  has_many :posts, through: :posts_tags
+
+  validates :name, presence: true, uniqueness: true
 end
